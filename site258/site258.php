3408a45f647e77d194d10ec4f69f45514769f18c
Thiago Senna <trsenna@gmail.com>
2015-08-20 00:21:49 -0300

    saving progress

---
+++ site258/site258.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: Site258 Functionality
+ * Plugin URI: https://github.com/trsenna/gaboo-skeleton
+ * Description: Unique and specific functionality needed for the current website.
+ *
+ * Version: 0.1.0
+ * Author: Thiago Senna
+ * Author URI: http://thremes.com.br
+ *
+ * @package   Site258Functionality
+ * @author    Thiago Senna <thiago@thremes.com.br>
+ * @copyright Copyright (c) 2015, Thiago Senna
+ * @license   http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ */
