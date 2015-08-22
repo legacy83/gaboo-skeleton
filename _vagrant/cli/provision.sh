7c23031368bc5fdfc94d4934ec4e663cbe23675b
Thiago Senna <trsenna@gmail.com>
2015-08-21 23:55:30 -0300

    local provision only

--- _vagrant/cli/provision.sh
+++ _vagrant/cli/provision.sh
@@ -1,44 +1,14 @@
 #!/bin/sh
 
+wp_location='/var/www/www.192.168.27.14.xip.io/public_html'
+
 ###########################################################
-# Cli Provision
+# WordPress Setup
 ###########################################################
 
-domain='192.168.27.14.xip.io'
+cd "$wp_location" && /opt/wp-cli/bin/wp core update
+cd "$wp_location" && /opt/wp-cli/bin/wp plugin update --all
 
-apache2AddVhost="$(curl -sS https://raw.githubusercontent.com/trsenna/gozma14/0.5.0/cli/stuff/apache2/addVhost.sh)"
-apache2Add301Redirect="$(curl -sS https://raw.githubusercontent.com/trsenna/gozma14/0.5.0/cli/stuff/apache2/add301Redirect.sh)"
-mySQLCreateDB="$(curl -sS https://raw.githubusercontent.com/trsenna/gozma14/0.5.0/cli/stuff/mySQL/createDB.sh)"
-
-##########################################
-# WWW Setup
-##########################################
-
-echo "$apache2AddVhost" | bash -s "$domain"
-echo "$apache2AddVhost" | bash -s "www.$domain"
-echo "$apache2Add301Redirect" | bash -s "$domain" "www.$domain"
-
-##########################################
-# Skeleton Project Setup
-##########################################
-
-echo "$mySQLCreateDB" | bash -s "skeleton"
-
-rm -rf "/var/www/www.$domain/public_html"
-ln -s /vagrant/projects/skeleton "/var/www/www.$domain/public_html"
-
-##########################################
-# Sample Project Setup
-##########################################
-
-#echo "$mySQLCreateDB" | bash -s "sample"
-#echo "$apache2AddVhost" | bash -s "sample.$domain"
-
-#rm -rf "/var/www/sample.$domain/public_html"
-#ln -s /vagrant/projects/sample "/var/www/sample.$domain/public_html"
-
-##########################################
-# Apache2 Reload
-##########################################
-
-service apache2 reload
+cd "$wp_location" && /opt/wp-cli/bin/wp plugin install regenerate-thumbnails
+cd "$wp_location" && /opt/wp-cli/bin/wp plugin install wordpress-importer
+cd "$wp_location" && /opt/wp-cli/bin/wp theme update --all
