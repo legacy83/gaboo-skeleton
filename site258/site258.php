2171143b6ed7a19bebc4aa9f3dc6681bf315db03
Thiago Senna <trsenna@gmail.com>
2015-08-26 23:03:11 -0300

    it's not 0.2.0 on skeleton

--- site258/site258.php
+++ site258/site258.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://github.com/trsenna/gaboo-skeleton
  * Description: Unique and specific functionality needed for the current website.
  *
- * Version: 0.2.0
+ * Version: 0.1.0
  * Author: Thiago Senna
  * Author URI: http://thremes.com.br
  *
